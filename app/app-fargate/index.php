8918729aede973e26d512872025f256f7d216ae4
mvtthxw <medykowski.mateusz@gmail.com>
2026-06-10 19:07:31 +0000

    refactor: change parameter function from faragate to managed

--- app/app-fargate/index.php
+++ app/app-fargate/index.php
@@ -5,18 +5,9 @@
     <title>App Fargate</title>
 </head>
 <body>
-    <h1>Param Store</h1>
+    <h1>Time App</h1>
     <h1>Hello from App Fargate <?= htmlspecialchars(is_readable('/etc/app/version') ? trim(file_get_contents('/etc/app/version')) : 'dev') ?></h1>
     <p>Container: <?= htmlspecialchars(gethostname()) ?></p>
-    <p>Param Store:
-        <?php
-        $paramStore = getenv('PARAMS_STORE');
-        if ($paramStore) {
-            echo htmlspecialchars($paramStore);
-        } else {
-            echo '<i>not set</i>';
-        }
-        ?>
-    </p>
+    <p>Time: <?= date('H:i:s') ?></p>
 </body>
 </html>
--- app/app-managed/index.php
+++ app/app-managed/index.php
@@ -5,9 +5,18 @@
     <title>App Managed</title>
 </head>
 <body>
-    <h1>Time App</h1>
+    <h1>Param Store</h1>
     <h1>Hello from App Managed <?= htmlspecialchars(is_readable('/etc/app/version') ? trim(file_get_contents('/etc/app/version')) : 'dev') ?></h1>
     <p>Container: <?= htmlspecialchars(gethostname()) ?></p>
-    <p>Time: <?= date('H:i:s') ?></p>
+    <p>Param Store:
+        <?php
+        $paramStore = getenv('PARAMS_STORE');
+        if ($paramStore) {
+            echo htmlspecialchars($paramStore);
+        } else {
+            echo '<i>not set</i>';
+        }
+        ?>
+    </p>
 </body>
 </html>
